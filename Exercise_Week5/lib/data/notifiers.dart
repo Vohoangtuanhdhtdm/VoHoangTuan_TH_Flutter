d95b14ec715a8d484dd31c55f95a52a6fec1e255
Vohoangtuanhdhtdm <vohoangtuan1980t@gmail.com>
2025-12-04 21:40:11 +0700

    Done App Week 5

--- Exercise_Week5/lib/data/notifiers.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-ValueNotifier<int> selectPageNotifier = ValueNotifier(0);
---
+++ Exercise_Week5/lib/models/note.dart
@@ -0,0 +1,35 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  // Chuyển object sang Map để lưu vào SQLite
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'title': title,
+    'content': content,
+    'createdAt': createdAt.toIso8601String(),
+    'updatedAt': updatedAt.toIso8601String(),
+  };
+
+  // Chuyển Map từ SQLite sang object
+  static Note fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      content: map['content'],
+      createdAt: DateTime.parse(map['createdAt']),
+      updatedAt: DateTime.parse(map['updatedAt']),
+    );
+  }
+}
